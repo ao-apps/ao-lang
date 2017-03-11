db5705872ece86e9e66ece721ba99020c22917d9
AO Industries, Inc <support@aoindustries.com>
2017-03-10 19:13:30 -0600

    New ao-lang project being pulled out of monolithic aocode-public project

---
+++ README.md
@@ -0,0 +1,32 @@
+# [<img src="ao-logo.png" alt="AO Logo" width="35" height="40">](https://aoindustries.com/) [AO Lang](https://aoindustries.com/ao-lang/)
+Minimal Java library with no external dependencies shared by many other projects.
+
+## Features
+* Small, simple, unfocused utilities that are generally extensions to the standard Java libraries.
+* Shared by many other projects and can be used to glue them together.
+* Small footprint, self-contained, no transitive dependencies - not part of a big monolithic package.
+* Android compatible.
+* Java EE 6+ compatible.
+
+## Motivation
+As we untangle our monolitic [AOCode Public](https://aoindustries.com/aocode-public/) project, we are placing small, simple, and unfocused utilities into this new project.  These tools are generally extensions to the standard Java libraries.  The `ao-lang` project is used by many other projects, but does not have any dependencies itself.
+
+## Alternatives
+There may be functionality that is also found in other projects.  This project is of a different evolutionary origin.
+* [Apache Commons Lang](https://commons.apache.org/proper/commons-lang/) - A popular set of extra methods generally extending the standard Java libraries.
+* [Google Guava](https://github.com/google/guava) - Google Core Libraries for Java.
+
+## Project Links
+* [Project Home](https://aoindustries.com/ao-lang/)
+* [Changelog](https://aoindustries.com/ao-lang/changelog)
+* [API Docs](https://aoindustries.com/ao-lang/apidocs/)
+* [Maven Central Repository](https://search.maven.org/#search%7Cgav%7C1%7Cg:%22com.aoindustries%22%20AND%20a:%22ao-lang%22)
+* [GitHub](https://github.com/aoindustries/ao-lang)
+
+## Contact Us
+For questions or support, please [contact us](https://aoindustries.com/contact):
+
+Email: [support@aoindustries.com](mailto:support@aoindustries.com)  
+Phone: [1-800-519-9541](tel:1-800-519-9541)  
+Phone: [+1-251-607-9556](tel:+1-251-607-9556)  
+Web: https://aoindustries.com/contact
---
+++ src/main/java/com/aoindustries/lang/ObjectUtils.java
@@ -0,0 +1,81 @@
+/*
+ * ao-lang - Minimal Java library with no external dependencies shared by many other projects.
+ * Copyright (C) 2012, 2013, 2016, 2017  AO Industries, Inc.
+ *     support@aoindustries.com
+ *     7262 Bull Pen Cir
+ *     Mobile, AL 36695
+ *
+ * This file is part of ao-lang.
+ *
+ * ao-lang is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ao-lang is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ao-lang.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.aoindustries.lang;
+
+import java.util.Arrays;
+
+/**
+ * Utilities that help when working with objects.
+ *
+ * @author  AO Industries, Inc.
+ */
+public final class ObjectUtils {
+
+	/**
+	 * Make no instances.
+	 */
+	private ObjectUtils() {
+	}
+
+	/**
+	 * Gets the hashCode for an object or <code>0</code> when <code>null</code>.
+	 * 
+	 * @see java.util.Objects#hashCode(java.lang.Object) as of Java 1.7
+	 * 
+	 * Java 1.7: deprecated  use java.util.Objects#hashCode(Object) as of Java 1.7
+	 */
+	// Java 1.7: @Deprecated
+	public static int hashCode(Object obj) {
+		return obj != null ? obj.hashCode() : 0;
+	}
+
+	/**
+	 * Gets the hashCode for a set of objects or <code>0</code> when <code>null</code>.
+	 * 
+	 * @see java.util.Objects#hash(java.lang.Object...) as of Java 1.7
+	 * 
+	 * Java 1.7: deprecated  use java.util.Objects#hash as of Java 1.7
+	 */
+	// Java 1.7: @Deprecated
+	public static int hash(Object... values) {
+		return Arrays.hashCode(values);
+	}
+	/**
+	 * Compares the equality of two objects, including their null states.
+	 * 
+	 * @see java.util.Objects#equals(java.lang.Object, java.lang.Object) as of Java 1.7
+	 * 
+	 * Java 1.7: deprecated  use java.util.Objects#equals(Object, Object) as of Java 1.7
+	 */
+	// Java 1.7: @Deprecated
+	public static boolean equals(Object obj1, Object obj2) {
+		return (obj1 == obj2) || (obj1 != null && obj1.equals(obj2));
+	}
+
+	/**
+	 * Calls toString if non-null, returns null when null.
+	 */
+	public static String toString(Object obj) {
+		return obj==null ? null : obj.toString();
+	}
+}
