5ecb7dc52982b6903697d72fb159cce21bf7cc6c
AO Industries, Inc. <support@aoindustries.com>
2020-01-19 22:06:08 -0600

    New appendTo(…) methods on Writable.

---
+++ src/main/java/com/aoindustries/io/AppendableWriter.java
@@ -0,0 +1,116 @@
+/*
+ * ao-lang - Minimal Java library with no external dependencies shared by many other projects.
+ * Copyright (C) 2020  AO Industries, Inc.
+ *     support@aoindustries.com
+ *     7262 Bull Pen Cir
+ *     Mobile, AL 36695
+ *
+ * This file is part of ao-lang.
+ *
+ * ao-lang is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ao-lang is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ao-lang.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.aoindustries.io;
+
+import java.io.IOException;
+import java.io.Writer;
+import javax.swing.text.Segment;
+
+/**
+ * Writer that writes to an underlying {@link Appendable}.
+ * This is not necessarily the absolute fastest way to get things done (at least
+ * when a {@code char[]} has to be wrapped in a {@link Segment}), but can help
+ * bridge the gap between APIs based on {@link Writer} and {@link Appendable}.
+ *
+ * @author  AO Industries, Inc.
+ */
+public class AppendableWriter extends Writer {
+
+	/**
+	 * Wraps the given {@link Appendable} if it is not already
+	 * a {@link Writer}.
+	 *
+	 * @return  The given out, if it is already a {@link Writer}, otherwise a
+	 *          {@link AppendableWriter} wrapping out.
+	 */
+	public static Writer wrap(Appendable out) {
+		if(out instanceof Writer) return (Writer)out;
+		return new AppendableWriter(out);
+	}
+
+	private final Appendable out;
+
+	public AppendableWriter(Appendable out) {
+		this.out = out;
+	}
+
+	/**
+	 * Gets the wrapped appendable.
+	 */
+	public Appendable getOut() {
+		return out;
+	}
+
+	@Override
+    public void write(int c) throws IOException {
+		out.append((char)c);
+	}
+
+	@Override
+	public void write(char cbuf[]) throws IOException {
+		out.append(new Segment(cbuf, 0, cbuf.length));
+	}
+
+	@Override
+	public void write(char cbuf[], int off, int len) throws IOException {
+		out.append(new Segment(cbuf, off, len));
+	}
+
+	@Override
+	public void write(String str) throws IOException {
+		out.append(str);
+	}
+
+	@Override
+	public void write(String str, int off, int len) throws IOException {
+		out.append(str, off, off + len);
+	}
+
+	@Override
+	public AppendableWriter append(CharSequence csq) throws IOException {
+		out.append(csq);
+		return this;
+	}
+
+	@Override
+	public AppendableWriter append(CharSequence csq, int start, int end) throws IOException {
+		out.append(csq, start, end);
+		return this;
+	}
+
+	@Override
+	public AppendableWriter append(char c) throws IOException {
+		out.append(c);
+		return this;
+	}
+
+	@Override
+	public void flush() throws IOException {
+		// Do nothing
+	}
+
+	@Override
+	public void close() throws IOException {
+		// Do nothing
+	}
+}
--- src/main/java/com/aoindustries/io/Writable.java
+++ src/main/java/com/aoindustries/io/Writable.java
@@ -1,6 +1,6 @@
 /*
  * ao-lang - Minimal Java library with no external dependencies shared by many other projects.
- * Copyright (C) 2013, 2015, 2016, 2017, 2019  AO Industries, Inc.
+ * Copyright (C) 2013, 2015, 2016, 2017, 2019, 2020  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -41,7 +41,7 @@ public interface Writable  {
 	/**
 	 * Checks if the writable will be able to be converted toString in an extremely efficient manner.
 	 * This means without allocating any new buffer space or string copies.
-	 * Callers should prefer toString over writeTo when {@code isFastToString} returns true.
+	 * Callers should prefer {@code toString} over {@code writeTo} or {@code appendTo} when {@code isFastToString} returns true.
 	 * <p>
 	 * Note: As of Java 1.7.0_06, {@link String#substring(int, int)} and related operations now
 	 * copy underlying buffers.
@@ -51,8 +51,8 @@ public interface Writable  {
 
 	/**
 	 * Anything writable must have a {@code toString} consistent with what would be
-	 * written by the {@code writeTo} methods.  For larger amounts of data, it is
-	 * likely much more efficient to call the most appropriate {@code writeTo} method.
+	 * written by the {@code writeTo} and {@code appendTo} methods.  For larger amounts of data, it is
+	 * likely much more efficient to call the most appropriate {@code writeTo} or {@code appendTo} method.
 	 */
 	@Override
 	String toString();
@@ -88,6 +88,90 @@ public interface Writable  {
 	 * @param  encoder  if null, no encoding is performed and will be the same as a call to {@link #writeTo(java.io.Writer, long, long)}
 	 */
 	void writeTo(Encoder encoder, Writer out, long off, long len) throws IOException;
+
+	/**
+	 * Appends a streamed version of the object's String representation.
+	 * What is appended must be the same as if {@code out.append(this.toString())}
+	 * were called, but may be a much more efficient implementation.
+	 */
+	default void appendTo(Appendable out) throws IOException {
+		assert out != null;
+		if(out instanceof Writer) {
+			writeTo((Writer)out);
+		} else if(isFastToString()) {
+			out.append(toString());
+		} else {
+			writeTo(new AppendableWriter(out));
+		}
+	}
+
+	/**
+	 * Appends a streamed version of the object's String representation.
+	 * What is appended must be the same as if {@code out.append(this.toString(), start, end)}
+	 * were called, but may be a much more efficient implementation.
+	 */
+	default void appendTo(Appendable out, long start, long end) throws IOException {
+		assert out != null;
+		if(out instanceof Writer) {
+			writeTo((Writer)out, start, end - start);
+		} else if(
+			   start >= Integer.MIN_VALUE
+			&& start <= Integer.MAX_VALUE
+			&& end >= Integer.MIN_VALUE
+			&& end <= Integer.MAX_VALUE
+			&& isFastToString()
+		) {
+			out.append(toString(), (int)start, (int)end);
+		} else {
+			writeTo(new AppendableWriter(out), start, end - start);
+		}
+	}
+
+	/**
+	 * Appends a streamed version of the object's String representation using the given encoder.
+	 * What is appended must be the same as if {@code encoder.append(this.toString(), out)}
+	 * were called, but may be a much more efficient implementation.
+	 * 
+	 * @param  encoder  if {@code null}, no encoding is performed and will be the same as a call to {@link #appendTo(java.lang.Appendable)}
+	 */
+	default void appendTo(Encoder encoder, Appendable out) throws IOException {
+		assert out != null;
+		if(encoder == null) {
+			appendTo(out);
+		} else if(out instanceof Writer) {
+			writeTo(encoder, (Writer)out);
+		} else if(isFastToString()) {
+			encoder.append(toString(), out);
+		} else {
+			writeTo(encoder, new AppendableWriter(out));
+		}
+	}
+
+	/**
+	 * Appends a streamed version of the object's String representation using the given encoder.
+	 * What is appended must be the same as if {@code encoder.append(this.toString(), start, end, out)}
+	 * were called, but may be a much more efficient implementation.
+	 * 
+	 * @param  encoder  if null, no encoding is performed and will be the same as a call to {@link #appendTo(java.lang.Appendable, long, long)}
+	 */
+	default void appendTo(Encoder encoder, Appendable out, long start, long end) throws IOException {
+		assert out != null;
+		if(encoder == null) {
+			appendTo(out, start, end);
+		} else if(out instanceof Writer) {
+			writeTo(encoder, (Writer)out, start, end - start);
+		} else if(
+			   start >= Integer.MIN_VALUE
+			&& start <= Integer.MAX_VALUE
+			&& end >= Integer.MIN_VALUE
+			&& end <= Integer.MAX_VALUE
+			&& isFastToString()
+		) {
+			encoder.append(toString(), (int)start, (int)end, out);
+		} else {
+			writeTo(encoder, new AppendableWriter(out), start, end - start);
+		}
+	}
 
 	/**
 	 * Trims the contents of this writable, returning the instance that represents this writable trimmed.
