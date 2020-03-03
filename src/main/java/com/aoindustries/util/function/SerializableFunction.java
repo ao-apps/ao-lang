9e45a1d282ba50df877613ff960a794e4f8dcacb
AO Industries, Inc. <support@aoindustries.com>
2020-03-03 16:32:06 -0600

    New @FunctionalInterface interface "SerializableFunction",
    that unions "Function" and "Serializable".

---
+++ src/main/java/com/aoindustries/util/function/SerializableFunction.java
@@ -0,0 +1,36 @@
+/*
+ * ao-lang - Minimal Java library with no external dependencies shared by many other projects.
+ * Copyright (C) 2020  AO Industries, Inc.
+ *     support@aoindustries.com
+ *     7262 Bull Pen Cir
+ *     Mobile, AL 36695
+ *
+ * This file is part of ao-lang.
+ *
+ * ao-lang is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ao-lang is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ao-lang.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.aoindustries.util.function;
+
+import java.io.Serializable;
+import java.util.function.Function;
+
+/**
+ * A function that must be {@link Serializable}.
+ * <p>
+ * Inspired by <a href="https://stackoverflow.com/questions/22807912/how-to-serialize-a-lambda">java - How to serialize a lambda? - Stack Overflow</a>.
+ * </p>
+ */
+@FunctionalInterface
+public interface SerializableFunction<T, R> extends Function<T, R>, Serializable {
+}
